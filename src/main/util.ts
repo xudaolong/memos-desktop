6dd9acbdb95972e0ca261e75d8a7bf057cb756b0
CorrectRoadH <a778917369@gmail.com>
2023-04-19 23:14:05 +0800

    eslint

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,11 +1,11 @@
 import { URL } from 'url';
 import Store from 'electron-store';
 
-export function resolveHtmlPath() {
+export default function resolveHtmlPath() {
   const port = process.env.PORT || 8081;
   const store = new Store();
 
-  const serverAddress = store.get('serverAddress');
+  const serverAddress = store.get('serverAddress') as string;
   const url = serverAddress
     ? new URL(serverAddress)
     : new URL(`http://localhost:${port}`);
